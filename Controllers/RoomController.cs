136ed42605ffdcfb74a6d1086abd391a3207f9a3
NguyenSonPhiHoang <93513347+Sindyzz@users.noreply.github.com>
2025-05-04 00:14:52 +0700

    GetRoomByFilter-GetAllRoomTypes

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -8,53 +8,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HotelManagement_BackEnd.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class RoomController : ControllerBase
+  [ApiController]
+[Route("api/[controller]")]
+public class RoomController : ControllerBase
+{
+    private readonly IRoomService _roomService;
+
+    public RoomController(IRoomService roomService)
     {
- 
-        private readonly IRoomService _roomService;
-
-        public RoomController(IRoomService roomService)
-        {
-            _roomService = roomService;
-        }
-
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var response = _roomService.GetAllAsync();
-            return Ok(response);
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult GetById(int id)
-        {
-            var response = _roomService.GetByIdAsync(id);
-            return Ok(response);
-        }
-
-        [HttpPost]
-        public IActionResult Create([FromBody] AddRoom room)
-        {
-            var response = _roomService.CreateAsync(room);
-            return Ok(response);
-        }
-
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, [FromBody] AddRoom room)
-        {
-            var response = _roomService.UpdateAsync(id, room);
-            return Ok(response);
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            var response = _roomService.DeleteAsync(id);
-            return Ok(response);
-        }
-
-
+        _roomService = roomService;
     }
+
+    [HttpGet("filter")]
+    public IActionResult GetRoomsByFilter(
+        [FromQuery] string trangThai = "Tất cả phòng",
+        [FromQuery] string loaiPhong = "Tất cả loại phòng",
+        [FromQuery] string tinhTrang = "Tất cả")
+    {
+        var result = _roomService.GetRoomsByFilter(trangThai, loaiPhong, tinhTrang);
+        return Ok(result);
+    }
+}
+
 }
--- Model/RoomType.cs
+++ Model/RoomType.cs
@@ -2,13 +2,8 @@ namespace HotelManagement_BackEnd.Model
 {
     public class RoomType
     {
-        public string MaLoaiPhong { get; set; }
+        public int MaLoaiPhong { get; set; }
         public string TenLoaiPhong { get; set; }
-        public ICollection<Room> Rooms { get; set; }
     }
 
-    public class AddRoomType
-    {
-        public string TenLoaiPhong { get; set; }
-    }
 }
